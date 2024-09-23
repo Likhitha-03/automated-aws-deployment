117529860ed264b6ca8b245c48e07972f709bb97
Likhitha Tangudu <86105341+Likhitha-03@users.noreply.github.com>
2024-09-23 12:01:15 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,3 +2,8 @@ output "instance_public_ip" {
   description = "The public IP of the EC2 instance"
   value       = aws_instance.new_instance.public_ip
 }
+
+output "ec2_private_key" {
+  value     = tls_private_key.pk.private_key_pem
+  sensitive = true  
+}
