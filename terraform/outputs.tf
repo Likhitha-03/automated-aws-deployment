99ab36b576a48fbaa1637574a7e879546701dcb9
Likhitha Tangudu <86105341+Likhitha-03@users.noreply.github.com>
2024-09-27 14:43:00 +0530

    Delete terraform/outputs.tf

--- terraform/outputs.tf
+++
@@ -1,8 +0,0 @@
-output "ec2_public_dns" {
-  description = "The public DNS of the EC2 instance"
-  value       = aws_instance.new_instance.public_dns
-}
-
-output "ec2_username" {
-  value = "ec2-user" 
-}
