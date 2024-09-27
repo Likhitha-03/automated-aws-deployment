4eefdd4ce18c75e5118a7f4bf25e046e4be1e914
Likhitha Tangudu <86105341+Likhitha-03@users.noreply.github.com>
2024-09-27 11:31:19 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,5 +1,6 @@
-output "ec2_public_ip" {
-  value = aws_instance.your_ec2_instance.public_ip
+output "instance_public_dns" {
+  description = "The public DNS of the EC2 instance"
+  value       = aws_instance.new_instance.public_dns
 }
 
 output "ec2_username" {
